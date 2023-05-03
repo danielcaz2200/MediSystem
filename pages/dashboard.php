ee23deb15c17c2b9047e6b2820a9f17dc0d613e8
Collin Chiu <collinchiu23@gmail.com>
2023-05-03 10:02:49 -0700

    Add mockup for dashboard page

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -15,6 +15,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="../styles.css?<?= time() ?>">
     <title>Dashboard</title>
 </head>
@@ -46,33 +47,21 @@
                 <div class="col">
                     <div class="card" style="width: 300px">
                         <div class="card-body text-center">
+                            <i class="bi bi-calendar4-event"></i>
                             <h3 class="card-title">Scheduling</h3>
-                            <p class="card-text">Your next appointment:</p>
-                            <a href="dashboard.php">None</a>
-                            <a href="dashboard.php">View your schedule</a>
+                            <p class="card-text"><strong>Your next appointment:</strong></p>
+                            <p>None</p>
+                            <a href="schedule.php">View your schedule</a>
                         </div> 
                     </div>
                 </div>
                 <div class="col">
                     <div class="card" style="width: 300px">
                         <div class="card-body text-center">
+                            <i class="bi bi-envelope"></i>
                             <h3 class="card-title">Messaging</h3>
-                            <p class="card-text"><?=1?> New Message</p>
-                            <p><strong>Recent Messages:</strong></p>
-                            <?php
-                                /*
-                                $user_name = $user_data['user_name'];
-
-                                $search_table = 'messages';
-                            
-                                $result = build_query($conn, $search_table);
-                            
-                                foreach ($result as $data) {
-                                    echo $data;
-                                }
-                                */
-                            ?>
-                            <p>Subject: <?="SUBJECT"?></p>
+                            <p class="card-text"><strong>Recent Messages:</strong></p>
+                            <p>None</p>
                             <a href="dashboard.php">View your Inbox</a>
                         </div> 
                     </div>
@@ -80,10 +69,11 @@
                 <div class="col">
                     <div class="card" style="width: 300px">
                         <div class="card-body text-center">
-                            <h3 class="card-title">Scheduling</h3>
-                            <p class="card-text">Your next appointment:</p>
-                            <span>None</span>
-                            <a href="dashboard.php">View your schedule</p>
+                            <i class="bi bi-search"></i>
+                            <h3 class="card-title">Search</h3>
+                            <p class="card-text"><strong>Recently viewed:</strong></p>
+                            <p>None</p>
+                            <a href="search.php">Search available clients</a>
                         </div> 
                     </div>
                 </div>
