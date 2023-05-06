b273e63d167bef74ad1cd35c527b00bbbf304005
Collin Chiu <collinchiu23@gmail.com>
2023-05-06 16:54:21 -0700

    Create messaging.php

---
+++ pages/messaging.php
@@ -0,0 +1,41 @@
+<?php
+    session_start();
+    include("../util/connection.php");
+    include("../util/functions.php");
+    
+    // check if user is logged in
+    $user_data = check_login($conn);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Messaging</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <ul class="navbar-nav me-auto">
+                <a class="navbar-brand" href="../index.php">MediSystem</a>
+                <li class="nav-item">
+                    <a class="nav-link" href="../index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./dashboard.php">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./search.php">Search</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./logout.php">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <h1 class="text-center" style="padding: 25px 0px 25px 0px">Messaging</h1>
+</body>
+</html>
